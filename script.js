d5080a1dee13a6a48752efdbf9a1dec8fb40e4a2
gajendra1221102 <02.y.gajender@gmail.com>
2023-09-27 11:35:02 +0530

    Initial Change

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('changeText').addEventListener('click', function () {
+    const paragraph = document.querySelector('p');
+    paragraph.textContent = 'Text changed using JavaScript!';
+});
